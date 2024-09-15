f9f860ec02df9b7891b28683ec9e611f5bd199e9
MontanDeev <ms.barrales.developerbackend@gmail.com>
2024-09-15 17:41:56 -0600

    agregue el headcomponent y el footcomponent

---
+++ src/components/FootComponent.js
@@ -0,0 +1,11 @@
+export default function FootComponent(){
+    return(
+        <div>
+            <footer className="footer">
+                <span className="text-muted">
+                    <h4 class="info">Equipo 5</h4>
+                </span>
+            </footer>
+        </div>
+    )
+}
---
+++ src/components/HeadComponent.js
@@ -0,0 +1,13 @@
+export default function HeadComponent() {
+    return (
+        <div>
+            <header className="header">
+                <nav className="navbar navbar-expand-md">
+                    <a href="/" className="navbar-brand">El Mapache Bigotón</a>
+                    <a href="/" className="navbar-brand">Inicio</a>
+                    <a href="/" className="navbar-brand">Citas</a>
+                </nav>
+            </header>
+        </div>
+    )
+}
